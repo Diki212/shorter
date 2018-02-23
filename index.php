f8164e5f96bbd4009dad438ce9bb7ae52dbb7ff6
Diki212 <36759318+Diki212@users.noreply.github.com>
2018-02-23 16:15:18 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,110 @@
+<?php
+
+?>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script src='http://www.geoplugin.net/javascript.gp' type='text/javascript'></script>
+<script type='text/javascript'>
+var link1 = "";
+var link2 = "";
+var link3 = "";
+switch(geoplugin_countryCode()){
+  case "US" :
+   if( navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/iPhone/i) ||
+    navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) ||
+    navigator.userAgent.match(/Windows Phone/i) || navigator.userAgent.match(/iPad/i))
+    {var urls = new Array("http://t.gminsane.com/aff_c?offer_id=3075&aff_id=394&aff_sub=fikri");
+     setTimeout("document.location = urls;",1500);
+    } else {var urls = new Array("http://t.gminsane.com/aff_c?offer_id=2669&aff_id=394&aff_sub=fikri");
+     setTimeout("document.location = urls;",1500);
+   }
+   break;
+  case "DE" :
+   if( navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/iPhone/i) ||
+    navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) ||
+    navigator.userAgent.match(/Windows Phone/i) || navigator.userAgent.match(/iPad/i))
+    {var urls = new Array("http://t.gminsane.com/aff_c?offer_id=2685&aff_id=394&aff_sub=fikri");
+     setTimeout("document.location = urls;",1500);
+    } else {var urls = new Array("http://t.gminsane.com/aff_c?offer_id=2809&aff_id=394&url_id=887&aff_sub=fikri");
+     setTimeout("document.location = urls;",1500);
+   }
+   break;
+  case "GB" :
+   if( navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/iPhone/i) ||
+    navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) ||
+    navigator.userAgent.match(/Windows Phone/i) || navigator.userAgent.match(/iPad/i))
+    {var urls = new Array("http://t.gminsane.com/aff_c?offer_id=3316&aff_id=394&aff_sub=fikri");
+     setTimeout("document.location = urls;",1500);
+    } else {var urls = new Array("http://t.gminsane.com/aff_c?offer_id=3128&aff_id=394&url_id=1008&aff_sub=fikri");
+     setTimeout("document.location = urls;",1500);
+   }
+   break;
+  case "BE" :
+   if( navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/iPhone/i) ||
+    navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) ||
+    navigator.userAgent.match(/Windows Phone/i) || navigator.userAgent.match(/iPad/i))
+    {var urls = new Array("http://t.gminsane.com/aff_c?offer_id=3200&aff_id=394&aff_sub=fikri");
+     setTimeout("document.location = urls;",1500);
+    } else {var urls = new Array("http://t.gminsane.com/aff_c?offer_id=1785&aff_id=394&aff_sub=fikri");
+     setTimeout("document.location = urls;",1500);
+   }
+   break;
+  case "NL" :
+   if( navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/iPhone/i) ||
+    navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) ||
+    navigator.userAgent.match(/Windows Phone/i) || navigator.userAgent.match(/iPad/i))
+    {var urls = new Array("http://t.gminsane.com/aff_c?offer_id=1029&aff_id=394&aff_sub=fikri");
+     setTimeout("document.location = urls;",1500);
+    } else {var urls = new Array("http://t.gminsane.com/aff_c?offer_id=3101&aff_id=394&aff_sub=fikri");
+     setTimeout("document.location = urls;",1500);
+   }
+   break;
+  case "AT" :
+   if( navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/iPhone/i) ||
+    navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) ||
+    navigator.userAgent.match(/Windows Phone/i) || navigator.userAgent.match(/iPad/i))
+    {var urls = new Array("http://t.gminsane.com/aff_c?offer_id=3087&aff_id=394&aff_sub=fikri");
+     setTimeout("document.location = urls;",1500);
+    } else {var urls = new Array("http://t.gminsane.com/aff_c?offer_id=3059&aff_id=394&aff_sub=fikri");
+     setTimeout("document.location = urls;",1500);
+   }
+   break;
+  case "AU" :
+   if( navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/iPhone/i) ||
+    navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) ||
+    navigator.userAgent.match(/Windows Phone/i) || navigator.userAgent.match(/iPad/i))
+    {var urls = new Array("http://t.gminsane.com/aff_c?offer_id=3132&aff_id=394&aff_sub=fikri");
+     setTimeout("document.location = urls;",1500);
+    } else {var urls = new Array("http://t.gminsane.com/aff_c?offer_id=2965&aff_id=394&aff_sub=fikri");
+     setTimeout("document.location = urls;",1500);
+   }
+   break;
+  case "FR" :
+   if( navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/iPhone/i) ||
+    navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) ||
+    navigator.userAgent.match(/Windows Phone/i) || navigator.userAgent.match(/iPad/i))
+    {var urls = new Array("http://t.gminsane.com/aff_c?offer_id=2345&aff_id=394&aff_sub=fikri");
+     setTimeout("document.location = urls;",1500);
+    } else {var urls = new Array("http://t.gminsane.com/aff_c?offer_id=2347&aff_id=394&aff_sub=fikri");
+     setTimeout("document.location = urls;",1500);
+   }
+   break;
+  default :
+   if( navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/iPhone/i) ||
+    navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) ||
+    navigator.userAgent.match(/Windows Phone/i) || navigator.userAgent.match(/iPad/i))
+    {var urls = new Array("http://www.google.com");
+     setTimeout("document.location = urls;",1500);
+    } else {var urls = new Array("http://www.google.com");
+     setTimeout("document.location = urls;",1500);
+   }
+   break;
+}
+</script>
+</head>
+<body>
+
+<p>Loading.....</p>
+</body>
+</html>
